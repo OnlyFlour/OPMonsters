565a122e09e9f96e3f3d0eb1003e6a81ac6bc588
Johan2403 <johanemman243@gmail.com>
2020-11-13 13:04:48 +0530

    Initial commit for 1.16.3

---
+++ src/main/java/johan24/opmonsters/OPMonsters.java
@@ -0,0 +1,30 @@
+package johan24.opmonsters;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(OPMonsters.MODID)
+public class OPMonsters {
+    public static final Logger LOGGER = LogManager.getLogger();
+    public static final String MODID = "opmonsters";
+
+    public OPMonsters() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        LOGGER.info("Hi from OP Monsters!");
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+}
