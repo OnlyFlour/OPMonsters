2cd5539d099d794e794ce8fe47ad18a10ff9d208
Johan2403 <johanemman243@gmail.com>
2021-01-07 18:44:23 +0530

    Finally added working configs.

--- src/main/java/johan24/opmonsters/OPMonsters.java
+++ src/main/java/johan24/opmonsters/OPMonsters.java
@@ -4,12 +4,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import johan24.opmonsters.client.ClientReference;
+import johan24.opmonsters.config.OPMonstersConfig;
 import johan24.opmonsters.server.dedicated.DedicatedServerReference;
 import johan24.opmonsters.util.ISidedReference;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -30,6 +33,7 @@ public class OPMonsters {
     	final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus(), forgeEventBus = MinecraftForge.EVENT_BUS;
         SIDED_SYSTEM.setup(modEventBus, forgeEventBus);
         modEventBus.addListener(this::setup);
+        ModLoadingContext.get().registerConfig(Type.COMMON, OPMonstersConfig.commonSpec);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
---
+++ src/main/java/johan24/opmonsters/config/OPMonstersConfig.java
@@ -0,0 +1,70 @@
+package johan24.opmonsters.config;
+
+import org.apache.commons.lang3.tuple.Pair;
+
+import johan24.opmonsters.OPMonsters;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.ForgeConfigSpec.BooleanValue;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.config.ModConfig;
+
+@EventBusSubscriber(modid = OPMonsters.MODID, bus = Bus.MOD)
+public class OPMonstersConfig {
+	public static final ForgeConfigSpec commonSpec;
+	public static final Common COMMON;
+	
+	static {
+		final Pair<Common, ForgeConfigSpec> specPair = new ForgeConfigSpec.Builder().configure(Common::new);
+		commonSpec = specPair.getRight();
+		COMMON = specPair.getLeft();
+	}
+	
+	public static class Common {
+		public final BooleanValue spawnZombieKnights;
+		public final BooleanValue spawnSkeletonArchers;
+		public final BooleanValue spawnChargedCreepers;
+		
+		Common(final ForgeConfigSpec.Builder builder) {
+			builder.comment("Contains settings related to zombies.")
+			.push("Zombie Settings");
+			
+			spawnZombieKnights = builder.comment("Set to true if you want to spawn Zombie Knights")
+					                    .translation("opmonsters.config.common.zombie.spawnZombieKnights")
+					                    .worldRestart()
+					                    .define("spawnZombieKnights", true);
+			builder.pop();
+			
+			builder.comment("Contains settings related to skeletons.")
+			.push("Skeleton Settings");
+			
+			spawnSkeletonArchers = builder.comment("Set to true if you want to spawn Skeleton Archers")
+					                      .translation("opmonsters.config.common.skeleton.spawnSkeletonArchers")
+					                      .worldRestart()
+					                      .define("spawnSkeletonArchers", true);
+			
+			builder.pop();
+			
+			builder.comment("Contains settings related to creepers.")
+			.push("Creeper Settings");
+			
+			spawnChargedCreepers = builder.comment("Set to true if you want to spawn Charged Creepers")
+					                      .translation("opmonsters.config.common.creeper.spawnChargedCreepers")
+					                      .worldRestart()
+					                      .define("spawnChargedCreepers", true);
+			
+			builder.pop();
+		}
+	}
+	
+	@SubscribeEvent
+    public static void onLoad(final ModConfig.Loading configEvent) {
+        OPMonsters.LOGGER.debug("Loaded OPMonsters Config file {}", configEvent.getConfig().getFileName());
+    }
+
+    @SubscribeEvent
+    public static void onFileChange(final ModConfig.Reloading configEvent) {
+        OPMonsters.LOGGER.debug("OPMonsters Config just got changed on the file system!");
+    }
+}
